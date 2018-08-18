3814022aa9d25e773052de4a72f6095a06b43045
taoqi <neother@126.com>
2018-08-18 22:40:58 +0800

    finished the charpter4

--- Flask/1.py
+++ Flask/1.py
@@ -9,10 +9,10 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-
+from flask import Flask, render_template, session, redirect, url_for, flash
 app = Flask(__name__)
 #manager = Manager(app)
 bootstrap = Bootstrap(app)
@@ -20,14 +20,23 @@ moment = Moment(app)
 app.config['SECRET_KEY'] = 'hardtoguess'
 
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+        #form.name.data = ''
+
+    return render_template('index.html', name=session.get('name'), form=form, current_time=datetime.utcnow())
 
 
 @app.route('/user/<id>')
