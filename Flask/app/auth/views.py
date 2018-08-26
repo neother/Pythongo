a51aefe5e1f79eb90ac60b6f5adddcae23b483ac
taoqi <neother@126.com>
2018-08-27 01:42:36 +0800

    completed 8.4.3

---
+++ Flask/app/auth/views.py
@@ -0,0 +1,26 @@
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user
+
+from . import auth
+from ..models import User
+from .forms import LoginForm
+
+from flask_login import login_required
+
+
+@auth.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password.')
+    return render_template('auth/login.html', form=form)
+
+
+@app.route('/secret')
+@login_required
+def secret():
+    return 'Only authenticated users are allowed!'
--- Flask/app/models.py
+++ Flask/app/models.py
@@ -1,6 +1,7 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager
-from flask_login import login_required
 
 
 class User(UserMixin, db.Model):
@@ -11,13 +12,17 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-@app.route('/secret')
-@login_required
-def secret():
-    return 'Only authenticated users are allowed!'
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
