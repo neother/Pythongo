916ddc051b143a70c901679ad7d2236c6b74526d
taoqi <neother@126.com>
2018-09-25 15:43:16 +0800

    update the postbody line is changed to be 4

--- Flaskpractise/filepath.py
+++ Flaskpractise/filepath.py
@@ -1,13 +1,35 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+
+def path():
+    '''
+    abspath: return the absolute path
+    realpath: return the real path of a link file
+    dirname: Returns the directory of a path#
+    listdir: list all the elements in the dir
+
+    below result is :
+    C:/Users/moochergaga/project/Flaskpractise/filepath.py
+    C:/Users/moochergaga/project/Flaskpractise
+    C:/Users/moochergaga/project/Flaskpractise
+    ['filepath.py', 'hello.py', 'mailtest.py', 'matplotlib', 'templates']
+    C:/Users/moochergaga/project/Flaskpractise/filepath.py
+    C:/Users/moochergaga/project/Flaskpractise/hello.py
+    C:/Users/moochergaga/project/Flaskpractise/mailtest.py
+    C:/Users/moochergaga/project/Flaskpractise/matplotlib
+    C:/Users/moochergaga/project/Flaskpractise/templates
+
+    '''
+
+    print(os.path.abspath(__file__))
+    print(os.path.dirname(os.path.abspath(__file__)))
+    print(os.path.dirname(os.path.realpath(__file__)))
+
+    filelist = os.listdir('C:/Users/moochergaga/project/Flaskpractise')
+    print(filelist)
+
+    for file in filelist:
+        print((os.path.realpath(file)))
 
 
-# def abspath(path):
-# Return an absolute path.
-# def dirname(p):
-#Returns the directory component of a pathname#
-
-print(__file__)
-print(os.path.dirname(os.path.abspath(__file__)))
-print(os.path.dirname(os.path.realpath(__file__)))
+path()
